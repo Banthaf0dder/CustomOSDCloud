8a1a695e1686a1a1f90bc92bad917444a7daaa85
Banthaf0dder <jebrum.eagles@gmail.com>
2022-12-03 11:36:32 -0600

    Add files via upload

---
+++ Win10-21H2.ps1
@@ -0,0 +1,11 @@
+﻿Write-Host  -ForegroundColor Yellow "Starting Custom OSDCloud..."
+cls
+
+Write-Host  -ForegroundColor Yellow "Loading OSDCloud..."
+
+Import-Module OSD -Force
+Install-Module OSD -Force
+
+Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Enterprise -ZTI
+
+wpeutil reboot
