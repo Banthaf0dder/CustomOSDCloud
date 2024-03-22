639093987c0a0207665e09bac7530f7e66b70da8
Banthaf0dder <jebrum.eagles@gmail.com>
2024-03-22 13:28:35 -0500

    Update DellBIOSProviderTest.ps1

--- DellBIOSProviderTest.ps1
+++ DellBIOSProviderTest.ps1
@@ -3,3 +3,9 @@ Import-Module DellBIOSProvider -Force
 Set-Item -Path "DellSmbios:\SystemConfiguration\EmbSataRaid" -Value "Ahci"
 
 $ServiceTag = (Get-Item -Path "DellSmbios:\SystemInformation\SvcTag").CurrentValue
+
+(Get-Item -Path "DellSmbios:\SystemConfiguration\EmbSataRaid").CurrentValue
+
+Write-Host "$ServiceTag"
+
+Start-Sleep 10
